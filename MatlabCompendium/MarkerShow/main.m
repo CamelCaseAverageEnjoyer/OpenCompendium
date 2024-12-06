38ac4b7374ab00f736b763f7bb698a03ba366e9c
PernatiyZybr <orlov.vv@phystech.edu>
2024-12-06 16:02:31 +0300

    обновил матлаб

--- MatlabCompendium/MarkerShow/main.m
+++ MatlabCompendium/MarkerShow/main.m
@@ -1,32 +1,11 @@
-marker = [0 0 1 0;
-          0 1 0 0;
-          1 0 0 1;
-          0 0 0 1];
-marker_clr = [[0 0 0]; [1 1 1]];
-n = size(marker);
-n = n(1);
+hold on;
 
-x = zeros(4, n^2);
-y = zeros(4, n^2);
-z = zeros(4, n^2);
-c = zeros(1, n^2);
+r = [0; 0; 0];
+q = [1; 0; 0; 0];
+show_deputy(r, q);
 
-for i = 1:n
-    for j = 1:n
-        t = 0.01;
-        h = 1/n;
-%         v = [h*i+t, h*j+t, 0; 
-%              h*i+h-t, h*j+t, 0; 
-%              h*i+h-t, h*j+h-t, 0; 
-%              h*i+t, h*j+h-t, 0]; 
-%         fac = [1 2 3 4]; 
-%         patch('Vertices',v,'Faces',fac,'Color', 'green') 
+colormap('gray');
+lightangle(-45,30);
+view(45,45);
 
-        x(:, n*i+j) = [h*i+t; h*i+h-t; h*i+h-t; h*i+t];
-        y(:, n*i+j) = [h*j+t; h*j+t; h*j+h-t; h*j+h-t];
-        z(:, n*i+j) = [0; 0; 0; 0];
-        c(n*i+j) = marker_clr(marker(i,j) + 1, 1);
-    end
-end
-
-patch(x,y,z,c);
+hold off;
