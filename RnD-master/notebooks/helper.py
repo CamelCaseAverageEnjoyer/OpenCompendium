11ecebb66a055c60912b1ad1a239349ec6949336
PernatiyZybr <orlov.vv@phystech.edu>
2025-04-14 19:06:18 +0300

    1

--- RnD-master/notebooks/helper.py
+++ RnD-master/notebooks/helper.py
@@ -45,51 +45,96 @@ def v0req(w, t, r0, r1):
 
 def mycolor(a, c1=[7, 255, 224], c2=[218, 17, 255]):
     return tuple([(c1[i] * (1-a) + c2[i]*a)/255 for i in range(3)])
+#################################################
 
-w, t = var('w_0 t', real=True)
-r0 = kf.get_vars(name='r^0', n=3, numb=False)
-v0 = kf.get_vars(name='v^0', n=3, numb=False)
-r1 = kf.get_vars(name='r^1', n=3, numb=False)
-v1 = kf.get_vars(name='v^1', n=3, numb=False)
-r = [Function('r_x'), Function('r_y'), Function('r_z')]
-v = [Function('v_x'), Function('v_y'), Function('v_z')]
-a = a_orb(w, r[0](t), r[1](t), r[2](t), v[0](t), v[1](t), v[2](t))
+N_heatmap = 50
+rate = 10
+dx = 2
+x_list = np.linspace(-dx, dx, N_heatmap)
+y_list = np.linspace(-dx, dx, N_heatmap)
 
+d = 0.5
+dr = np.array([0.1, 0.1, 0])
+ro1, ro2 = np.array([-1, 0, 0]), np.array([1, 0, 0])
 
-dt = 1
-T = 5000
-w0 = o.v.W_ORB
-r0 = np.array([1, 0, 1])
-r1 = np.array([-1, 0, 0])
-v0 = v0req(w0, T, r0, r1)
+def point2line(p, r1, r2):
+    tau = (r2 - r1) / norm(r2 - r1)
+    kk = np.dot(tau, p - r1)  # Положение проекции точки P на линии отрезка
+    if 0<=kk<=norm(r2 - r1):  # Если проекция внутри отрезка
+        return norm(p - (r1 + kk*tau))
+    return -1
 
-z1 = lambda r, v, w: np.array([r[0] - 2*v[1]/w,
-                               r[1] + v[0]/2/w,
-                               0])
-z3 = lambda r, v, w: np.array([-w*(3*r[1]*v[2]*w + r[2]*v[1]*w + 2*v[0]*v[2]),
-                               2*w*(3*r[1]*r[2]*w**2 + 2*r[2]*v[0]*w - v[1]*v[2]),
-                               2*w*(9*r[1]**2*w**2 + 12*r[1]*v[0]*w + 4*v[0]**2 + v[1]**2)])
+def point2circle(p, r1, r2):
+    n = (r2 - r1) / norm(r2 - r1)
+    d1 = norm(p - r2)
+    cosa = np.dot(n, p - r2) / d1
+    sina = np.sqrt(1 - cosa**2)
+    kk = d1 * sina
+    if cosa >= 0:
+        if 0<=kk<=d:
+            return d1*cosa
+        if kk>d:
+            return np.sqrt(((d1)*cosa)**2 + ((d1-d)*sina)**2)
+    return -1
 
-fig = plt.figure(figsize=(6,6))
-axes = fig.add_subplot(projection='3d')
-rn = np.zeros((T // dt,3))
-for i in range(T // dt):
-    rn[i,:] = A(w0, dt*i) @ r0 + B(w0, dt*i) @ v0
-axes.scatter(*r0, c='b', label='Start point')
-axes.scatter(*r1, c='r', label='End point')
-axes.plot(*[rn[:,i] for i in range(3)], c='k', label='Trajectory')
-for j in range(T // dt):
-    if j % 100 == 0:
-        # bn1 = bn(r0, v0, w0, dt*j).T[0]
-        # bn1 /= np.linalg.norm(bn1)
-        # axes.plot(*[[rn[j,i], rn[j,i] + 0.5*bn1[i]] for i in range(3)], c='r', label=None)
-        
-        bn2 = z3(r0, v0, w0) # .T[0]
-        bn2 /= np.linalg.norm(bn2)
-        axes.plot(*[[rn[j,i], rn[j,i] + 0.5*bn2[i]] for i in range(3)], c='g')
-axes.legend()
-axes.set_xlabel("x, m")
-axes.set_ylabel("y, m")
-axes.set_zlabel("z, m")
-axes.set_aspect('equal')
+def h(r1, r2, ro1, ro2):
+    # Проверка расстояний от точек до торцов цилиндра
+    d1 = [point2circle(r1, ro1, ro2), 
+          point2circle(r2, ro1, ro2), 
+          point2circle(r1, ro2, ro1),
+          point2circle(r2, ro2, ro1),
+          point2circle(r1/2+r2/2, ro2, ro1),
+          point2circle(r1/2+r2/2, ro2, ro1)]
+    tmp = [i for i in d1 if i != -1]
+    anw = -1 if len(tmp) == 0 else min(tmp)
+    
+    # Проверка расстояний между отрезками
+    d2 = [point2line(r1, ro1, ro2) - d, 
+          point2line(r2, ro1, ro2) - d, 
+          point2line(ro1, r1, r2) - d,
+          point2line(ro2, r1, r2) - d
+         ]
+    tmp = [i for i in d2 if i > -1]
+    tmp = -1 if len(tmp) == 0 else min(tmp)
+    # anw = min(tmp, anw) if anw >=0 else tmp
+    if anw == -1 and tmp == -1:
+        pass
+    if anw == -1 and tmp != -1:
+        anw = tmp
+    if anw != -1 and tmp == -1:
+        anw = anw
+    if anw != -1 and tmp != -1:
+        anw = min(anw, tmp)
+
+    return np.clip(anw, 0, None)
+
+def k(x_):
+    return (x_ / (2*dx) + 0.5) * N_heatmap
+def k1(x_):
+    return (x_ / (2*dx)) * N_heatmap
+def k_(i_):
+    return (i_ / N_heatmap - 0.5) * (2*dx)
+anw = [[h(r1=np.array([k_(ix), k_(iy), 0]) - dr, 
+          r2=np.array([k_(ix), k_(iy), 0]) + dr,
+          ro1=ro1, ro2=ro2) for ix in range(N_heatmap)] for iy in range(N_heatmap)]
+
+fig, axes = plt.subplots(1, 1, figsize=(10, 8))
+v_x_list, v_z_list = [np.linspace(-dx, dx, N_heatmap) for _ in range(2)]
+xlabels_0, ylabels_0 = [['{:4.2f}'.format(x) for x in l] for l in [v_z_list, v_x_list]]
+ax = sns.heatmap(anw, ax=axes, cmap="crest", xticklabels=xlabels_0, yticklabels=ylabels_0,  cbar_kws={'label': 'Distance h, m'})
+ax.set_xticks(ax.get_xticks()[::rate]); ax.set_xticklabels(xlabels_0[::rate])
+ax.set_yticks(ax.get_yticks()[::rate]); ax.set_yticklabels(ylabels_0[::rate])
+ax.plot([k(ro1[0]), k(ro2[0])], [k(ro1[1]+d), k(ro2[1]+d)], c='c')
+ax.plot([k(ro1[0]), k(ro2[0])], [k(ro1[1]-d), k(ro2[1]-d)], c='c')
+ax.plot([k(ro1[0]), k(ro1[0])], [k(ro1[1]-d), k(ro1[1]+d)], c='c')
+ax.plot([k(ro2[0]), k(ro2[0])], [k(ro2[1]-d), k(ro2[1]+d)], c='c')
+ax.plot([k(ro1[0]), k(ro2[0])], [k(ro1[1]), k(ro2[1])], c='b')
+for ix in range(N_heatmap):
+    for iy in range(N_heatmap):
+        if ((ix+1) % 5 == 0) and ((iy+1) % 5 == 0):
+            ax.plot([ix-k1(dr[0]), ix+k1(dr[0])], [iy-k1(dr[1]), iy+k1(dr[1])], c='k')
+            # ax.plot([ix-(dr[0]), ix+(dr[0])], [iy-(dr[1]), iy+(dr[1])], c='k')
+ax.set_xlabel(f"x, m")
+ax.set_ylabel(f"y, m")
+ax.invert_yaxis()
 plt.show()
